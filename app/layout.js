2d41c50816ddc9fb4f90f3fb1bc322b3723f4d4b
Livsha <ddlivsha@gmail.com>
2025-06-12 15:34:14 +0300

    fonts + images

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import "./globals.sass";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Logistic</title>
+      </head>
       <body>
         {children}
       </body>
